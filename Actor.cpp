b22c7eacefad61c9d5507adc699dee7fd9a2a01d
Chris Brown <cbrown119955@gmail.com>
2016-02-15 12:35:50 -0800

    Implement basic version of FrackMan class

--- Actor.cpp
+++ Actor.cpp
@@ -4,16 +4,22 @@
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
 
 Actor::Actor(int imageId, int startX, int startY, Direction startDirection,
-      float size, unsigned int depth)
+	StudentWorld* world, float size, unsigned int depth)
   : GraphObject(imageId, startX, startY, startDirection, size, depth)
 {
+  m_world = world;
+}
+
+StudentWorld* Actor::getWorld()
+{
+  return m_world;
 }
 
 Actor::~Actor()
 {}
 
-Dirt::Dirt(int startX, int startY)
-  : Actor(IID_DIRT, startX, startY, right, 0.25, 3)
+Dirt::Dirt(int startX, int startY, StudentWorld* world)
+  : Actor(IID_DIRT, startX, startY, right, world, 0.25, 3)
 {
   setVisible(true);
 }
@@ -24,3 +30,92 @@ Dirt::~Dirt()
 //Dirt doesn't need to do anything ever
 void Dirt::doSomething()
 {}
+
+FrackMan::FrackMan(StudentWorld* world)
+  : Actor(IID_PLAYER, 30, 60, right, world, 1.0, 0)
+{
+  m_health = 10;
+  m_water = 5;
+  m_sonar = 1;
+  m_gold = 0;
+  setVisible(true);
+}
+
+FrackMan::~FrackMan()
+{}
+
+void FrackMan::doSomething()
+{
+  //TODO: Check if alive
+  //TODO: Check remove any dirt the FrackMan overlaps
+
+  //Check if a key is pressed; if so, act on it
+  int ch;
+  if(getWorld()->getKey(ch)) //note, needs access to the getKey() method in the GameWorld class
+    {
+      //user hit a key this tick!
+      switch (ch)
+	{
+	case KEY_PRESS_LEFT:
+	  processMovement(left);
+	  break;
+	case KEY_PRESS_RIGHT:
+	  processMovement(right);
+	  break;
+	case KEY_PRESS_UP:
+	  processMovement(up);
+	  break;
+	case KEY_PRESS_DOWN:
+	  processMovement(down);
+	  break;
+	case KEY_PRESS_SPACE:
+	  //TODO: Fire a squirt
+	  //+ other stuff, include playing sound
+	  break;
+	case KEY_PRESS_ESCAPE:
+	  //TODO: FrackMan sets itself to dead
+	  break;
+	case KEY_PRESS_TAB:
+	  //TODO: drop a gold nugget for the protesters
+	  break;
+	case 'z':
+	case 'Z':
+	  //Use a sonar
+	  break;
+	}
+    }
+}
+
+//If FrackMan is not facing in direction given, turn to face
+//that direction. Otherwise, try to move the FrackMan one
+//space in the direction he is facing.
+void FrackMan::processMovement(Direction moveDir)
+{
+  if(getDirection() != moveDir){
+    //Turn FrackMan to face in direction specified
+    setDirection(moveDir);
+  }
+  else{
+    //Try to move FrackMan one step in direction specified,
+    //ensuring that he does not exit the oil field
+    //TODO: Make sure FrackMan can't move near a boulder, etc.
+    if(moveDir == left){
+      if(getX() != 0)
+	moveTo(getX() - 1, getY());
+    }
+    else if(moveDir == right){
+      if(getX() != 60)
+	moveTo(getX() + 1, getY());
+    }
+    else if(moveDir == up){
+      if(getY() != 0)
+	moveTo(getX(), getY() - 1);
+    }
+    else if(moveDir == down){
+      if(getY() != 60)
+	moveTo(getX(), getY() + 1);
+    }
+  }
+
+  return;
+}
--- Actor.h
+++ Actor.h
@@ -5,23 +5,44 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class StudentWorld;
+
 class Actor : public GraphObject
 {
 public:
-  Actor(int imageId, int startX, int startY,
-	Direction startDirection, float size = 1.0, unsigned int depth = 0);
+  Actor(int imageId, int startX, int startY, Direction startDirection,
+	StudentWorld* world, float size, unsigned int depth);
   virtual ~Actor();
   virtual void doSomething() = 0;
+  StudentWorld* getWorld(); //TODO: See if this can be made private
 private:
+  StudentWorld* m_world;
 };
 
 class Dirt : public Actor
 {
 public:
-  Dirt(int startX, int startY);
+  Dirt(int startX, int startY, StudentWorld* world);
   ~Dirt();
   void doSomething();
 private:
+};
+
+//TODO: This class will probably end up deriving from
+//some other, more specific base class
+class FrackMan : public Actor
+{
+public:
+  FrackMan(StudentWorld* world);
+  ~FrackMan();
+  void doSomething();
+private:
+  void processMovement(Direction moveDir);
+  //TODO: Figure out which ones of these actually need to be here
+  int m_health;
+  int m_water;
+  int m_sonar;
+  int m_gold;
 };
 
 #endif // ACTOR_H_
