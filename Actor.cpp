d3b333e2a698b7da17e18eddba7ae690b8d84b9f
Chris Brown <cbrown119955@gmail.com>
2016-02-14 19:06:14 -0800

    Implement base class and Dirt in Actor.h and Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -2,3 +2,25 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+Actor::Actor(int imageId, int startX, int startY, Direction startDirection,
+      float size, unsigned int depth)
+  : GraphObject(imageId, startX, startY, startDirection, size, depth)
+{
+}
+
+Actor::~Actor()
+{}
+
+Dirt::Dirt(int startX, int startY)
+  : Actor(IID_DIRT, startX, startY, right, 0.25, 3)
+{
+  setVisible(true);
+}
+
+Dirt::~Dirt()
+{}
+
+//Dirt doesn't need to do anything ever
+void Dirt::doSomething()
+{}
--- Actor.h
+++ Actor.h
@@ -5,4 +5,23 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : public GraphObject
+{
+public:
+  Actor(int imageId, int startX, int startY,
+	Direction startDirection, float size = 1.0, unsigned int depth = 0);
+  virtual ~Actor();
+  virtual void doSomething() = 0;
+private:
+};
+
+class Dirt : public Actor
+{
+public:
+  Dirt(int startX, int startY);
+  ~Dirt();
+  void doSomething();
+private:
+};
+
 #endif // ACTOR_H_
